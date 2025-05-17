ece455c9c38017c43198fc6f21f8440a6d2ec324
vinceparks <vince22parks@gmail.com>
2025-05-17 15:36:51 -0500

    2025pycharmtestingproject7

--- data.py
+++ data.py
@@ -1,11 +1,11 @@
-# FileURBAN_ROUTES_URL with constants
+URBAN_ROUTES_URL= 'https://cnt-845358e6-0cce-4366-bf9c-0eed0541ec82.containerhub.tripleten-services.com/'''
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
 CARD_NUMBER = '1234 5678 9100'
 CARD_CODE = '1111'
 MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
-import data
+
 
 
 
