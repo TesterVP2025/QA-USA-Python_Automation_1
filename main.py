a6e2da95019e91a75426d08cb55ca17452a5a355
vinceparks <vince22parks@gmail.com>
2025-05-21 10:10:27 -0500

    2025pycharmtestingproject7-pt3

--- main.py
+++ main.py
@@ -11,35 +11,44 @@ class TestUrbanRoutes:
             print("Cannot connect to Urban Routes. Check the server is on and still running.")
 
     def test_set_route(self):
+        # Add in S8
         print("function created for set route")
-        # Your test code here
+        pass
 
     def test_select_plan(self):
+        # Add in S8
         print("function created for select plan")
-        # Your test code here
+        pass
 
     def test_fill_phone_number(self):
+        # Add in S8
         print("function created for fill phone number")
-        # Your test code here
+        pass
 
     def test_fill_card(self):
+        # Add in S8
         print("function created for fill card")
-        # Your test code here
+        pass
 
     def test_comment_for_driver(self):
+        # Add in S8
         print("function created for comment for driver")
-        # Your test code here
+        pass
 
     def test_order_blanket_and_handkerchiefs(self):
+        # Add in S8
         print("function created for order blanket and handkerchiefs")
-        # Your test code here
+        pass
 
     def test_order_2_ice_creams(self):
-        # Your test code here
+        # Add in S8
         print("function created for order 2 ice creams")
+        pass
 
     def test_car_search_model_appears(self):
+        # Add in S8
         print("function created for car search model appears")
-        # Your test code here
+        pass
+
 
 
