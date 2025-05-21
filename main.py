9ba5853fd537478df3f94984ef6b534179b0e4e1
vinceparks <vince22parks@gmail.com>
2025-05-20 19:28:02 -0500

    2025pycharmtestingproject7-pt3

--- main.py
+++ main.py
@@ -1,52 +1,47 @@
-import data
+from helpers import is_url_reachable
+from data import URBAN_ROUTES_URL
 
 class TestUrbanRoutes:
 
-    def test_set_route(self):
-        # Add in S8
-        print("function created for set route")
-        if data.is_url_reachable():
-            print("Server is running.")
+    @classmethod
+    def setup_class(cls):
+        if is_url_reachable(URBAN_ROUTES_URL):
+            print("Connected to the Urban Routes server.")
         else:
-            print("Server is NOT reachable.")
-        pass
+            print("Cannot connect to Urban Routes. Check the server is on and still running.")
+
+    def test_set_route(self):
+        print("function created for set route")
+        # Your test code here
 
     def test_select_plan(self):
-        # Add in S8
         print("function created for select plan")
-        pass
+        # Your test code here
 
     def test_fill_phone_number(self):
-        # Add in S8
         print("function created for fill phone number")
-        pass
+        # Your test code here
 
     def test_fill_card(self):
-        # Add in S8
         print("function created for fill card")
-        pass
+        # Your test code here
 
     def test_comment_for_driver(self):
-        # Add in S8
         print("function created for comment for driver")
-        pass
+        # Your test code here
 
     def test_order_blanket_and_handkerchiefs(self):
-        # Add in S8
         print("function created for order blanket and handkerchiefs")
-        pass
+        # Your test code here
 
     def test_order_2_ice_creams(self):
-        # Add in S8
         print("function created for order 2 ice creams")
         ice_cream_order = "vanilla"
         for count in range(2):
-            # Add in S8
             print(f"Ordering ice cream #{count + 1}: {ice_cream_order}")
-            pass
+            # Your test code here
 
     def test_car_search_model_appears(self):
-        # Add in S8
         print("function created for car search model appears")
-        pass
-print('test23')
+        # Your test code here
+
