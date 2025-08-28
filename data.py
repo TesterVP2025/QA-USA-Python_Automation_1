58c00240aab4f773a47c84b797ae07d92b0f4c92
vinceparks <vince22parks@gmail.com>
2025-08-28 18:41:43 -0500

    2025pycharmtestingproject7-pt3

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
 CARD_NUMBER = '1234 5678 9100'
 CARD_CODE = '1111'
-MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please' 
 
 
 
